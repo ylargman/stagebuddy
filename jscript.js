1d79187aabc14c3b019db3835cc8c12aed660215
Yan Largman <yanl@myth12.Stanford.EDU>
2012-10-23 17:00:58 -0700

    Create play page

---
+++ jscript.js
@@ -0,0 +1,21 @@
+$(document).live('pageinit', function(){
+	$("#generate").click(function(){
+		
+		numActs = $("#numActs").val();
+		html = '';
+		for (var i = 0; i < numActs; i++){
+			name = 'act' + i + 'NumScenes';
+			html += '<div data-role="fieldcontain">';
+			html += '<label>Act ' + i + ' number of scenes</label><input type="number" id="' + name + '" name="' + name + '"/>';
+			html += '</div>';
+		}
+		
+		html += '<div data-role="fieldcontain">';
+		html += '<input type="submit" id="playCreateButton" value="Create" />';
+		html += '</div>';
+		$('#scenePop').html(html);
+		$('#scenePop').popup("open");
+		$('.ui-page').trigger('create');
+	});
+})
+
