2c10ac58f4a4dc3ddd0bf157c921b63cfc4f7c47
Yan Largman <yanl@myth9.Stanford.EDU>
2012-12-04 22:07:23 -0800

    fixed popup backgrounds

--- login.php
+++ login.php
@@ -25,17 +25,15 @@
 	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
 	
 	<style type="text/css">
-		body {
+		.ui-page {
     		background: url(StageBuddyLowResBackground.png);
    			background-repeat:repeat-y;
     		background-position:center center;
     		background-attachment:scroll;
     		background-size:100% 100%;
 		}
-		.ui-page {
-    		background: transparent;
-		}
-		.ui-content{
+		
+		[data-role=content]{
     		background: transparent;
 		}
 	</style>
